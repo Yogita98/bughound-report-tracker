eef085d7dfe8544cdd08ec6db4fdb72cd3d6ee7a
Yogita98 <yogitab2798@gmail.com>
2024-04-12 22:30:05 -0700

    Update dump file

--- init.sql
+++ init.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.36, for macos14 (x86_64)
+-- MySQL dump 10.13  Distrib 8.3.0, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: bugreports
+-- Host: localhost    Database: bugreports
 -- ------------------------------------------------------
 -- Server version	8.3.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -285,7 +285,7 @@ CREATE TABLE `bughoundapp_employee` (
   UNIQUE KEY `Username` (`Username`),
   KEY `bughoundapp_employee_Role_id_54d785c8_fk_bughounda` (`Role_id`),
   CONSTRAINT `bughoundapp_employee_Role_id_54d785c8_fk_bughounda` FOREIGN KEY (`Role_id`) REFERENCES `bughoundapp_employeerole` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -294,7 +294,7 @@ CREATE TABLE `bughoundapp_employee` (
 
 LOCK TABLES `bughoundapp_employee` WRITE;
 /*!40000 ALTER TABLE `bughoundapp_employee` DISABLE KEYS */;
-INSERT INTO `bughoundapp_employee` VALUES (1,'John Doe','johndoe','password','john@example.com',1),(2,'Jane Doe','janedoe','pbkdf2_sha256$600000$swMAu4yOLszv8or2j6fLhB$kv5jn3jxXZRYW6dAkPQkhv9sbCLcShYS71fTrU1tQTQ=','jane@example.com',1),(3,'Yogita Bhatia','yogitabhatia','saval','ybhatia@example.com',3),(4,'Sharvika Kulkarni','sharvikakulkarni','goodgame','skulkarni@example.com',2),(5,'Riya Lunagariya','riyalunagariya','nikaloisko','rlunagariya@example.com',4),(6,'Sladyn','sladyn','pbkdf2_sha256$600000$1pOxVrvuJDTUOF3tj6wfgs$v4+anBRlRePofpDIYq6gYRPSLP6jUnNOu58vuS92j44=','vighnesh.khatri@gmail.com',2);
+INSERT INTO `bughoundapp_employee` VALUES (1,'John Doe','johndoe','password','john@example.com',1),(2,'Jane Doe','janedoe','pbkdf2_sha256$600000$swMAu4yOLszv8or2j6fLhB$kv5jn3jxXZRYW6dAkPQkhv9sbCLcShYS71fTrU1tQTQ=','jane@example.com',1),(3,'Yogita Bhatia','yogitabhatia','saval','ybhatia@example.com',3),(4,'Sharvika Kulkarni','sharvikakulkarni','goodgame','skulkarni@example.com',2),(5,'Riya Lunagariya','riyalunagariya','nikaloisko','rlunagariya@example.com',4),(6,'Sladyn','sladyn','pbkdf2_sha256$600000$1pOxVrvuJDTUOF3tj6wfgs$v4+anBRlRePofpDIYq6gYRPSLP6jUnNOu58vuS92j44=','vighnesh.khatri@gmail.com',2),(7,'Abhinav','abhinav','pbkdf2_sha256$600000$IuH6bwS0W6GbargkiMlssc$0xk35i/2t5ekDe417xEHqBqgtm4X0hG/bfiUglDixhc=','abhinav.chopra@gmail.com',1);
 /*!40000 ALTER TABLE `bughoundapp_employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -495,4 +495,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-10  1:03:50
+-- Dump completed on 2024-04-13  5:28:19
